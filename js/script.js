59c54d5595f35ac740951fa7faf0c7ef9c0ef65f
Nurasyl <Nurasylqazaqwws@gmail.com>
2023-01-29 12:41:00 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// "use strict";
+
+// let number = 4.6;
+
+// console.log(-4 / 0);
+// console.log('string' * 9);
+
+// const obj = {
+//      name: "John",
+//      age:25,
+//      isMaried:false
+// };
+
+// console.log(obj.age);
+
+
+// let arr = ['Nura', 'Madi', 'Nurik'];
+// console.log(arr);
+
+
+const questions=[]
+questions[0]=prompt('What is your name?', '');
+questions[1]=prompt('What is your surname?', '');
+questions[2]=prompt('How old are you?', '');
+console.log(questions);
